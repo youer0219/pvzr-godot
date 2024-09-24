b38f1d0b093a3f6a29ed69c98ecb321a9b058db8
徐鹿鸣 <xlm20040219@qq.com>
2024-09-24 11:14:53 +0800

    戴夫二段跳效果（待重构）

--- 场景/戴夫/移动测试.gd
+++ 场景/戴夫/移动测试.gd
@@ -8,39 +8,45 @@ var current_jump_times:int = jump_times
 @export var move_rotation_degrees:int = 15
 @export var rotation_change_speed:float = 40
 
+enum LateralOrientation {LEFT = 1,RIGHT = -1,IDEA = 0}
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+	var leteral_orientation:LateralOrientation 
 	
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * SPEED, 80)
+		small_jump()
+		$Sprite2D.flip_h = false if direction == 1 else true
+		$WallCheck.scale = Vector2(1,1) if direction == 1 else Vector2(-1,-1)
+		rotation_degrees = move_toward(rotation_degrees,move_rotation_degrees * direction,rotation_change_speed*delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, 80)
+	if !direction or ($WallCheck.is_colliding() and is_on_floor()):
+		rotation_degrees = move_toward(rotation_degrees,0,10)
+
 	if can_reset_jump_times():
 		current_jump_times = jump_times
 
 	# Handle jump.
 	if Input.is_action_just_pressed("move_up") and current_jump_times > 0:
 		velocity.y = JUMP_VELOCITY
+		if current_jump_times == 1:
+			make_turn($WallCheck.scale.x)
+			if !$WallCheck.is_colliding():
+				velocity.x += 600 * direction
 		current_jump_times -= 1
-
-
-	if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
-		var direction := Input.get_axis("move_left", "move_right")
-		if direction:
-			velocity.x = move_toward(velocity.x, direction * SPEED, 80)
-			small_jump()
-			if direction == 1:
-				$Sprite2D.flip_h = false
-				rotation_degrees = move_toward(rotation_degrees,move_rotation_degrees,rotation_change_speed*delta)
-			elif direction == -1:
-				$Sprite2D.flip_h = true
-				rotation_degrees = move_toward(rotation_degrees,-move_rotation_degrees,rotation_change_speed*delta)
-	else:
-		velocity.x = move_toward(velocity.x, 0, 80)
-		rotation_degrees = move_toward(rotation_degrees,0,10)
 	
 	move_and_slide()
 
+
 func small_jump():
-	if is_on_floor():
+	if is_on_floor() and !$WallCheck.is_colliding():
 		velocity.y = -150
 
 func can_reset_jump_times()->bool:
@@ -48,3 +54,8 @@ func can_reset_jump_times()->bool:
 		return true
 	
 	return false
+
+func make_turn(direction:int):
+	var tween = get_tree().create_tween()
+	tween.tween_property(self,"rotation_degrees",360*direction,0.3)
+	rotation_degrees = 0
