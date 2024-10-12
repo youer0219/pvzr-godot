2de0e442f9c3908fd667702e59968eb990337a2c
徐鹿鸣 <xlm20040219@qq.com>
2024-10-12 17:55:21 +0800

    寻路：完成Entity寻路中的获取路径方法

--- Scenes/Func/PathFinder/EntityPathFinder/entity_path_finder.gd
+++ Scenes/Func/PathFinder/EntityPathFinder/entity_path_finder.gd
@@ -8,8 +8,9 @@ var path_finder:MapPathFinder
 func _ready() -> void:
 	path_finder = get_tree().get_first_node_in_group("path_finder")
 
-func get_move_path():
-	pass
+func get_move_path(start_pos:Vector2,target_pos:Vector2)->Array[Vector2]:
+	return path_finder.get_move_path(start_pos,target_pos)
 
+# 暂时不搞这个
 func improve_move_path():
 	pass
