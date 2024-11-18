cc9d9329759f5047dff6fe668b2b565326a0b9ce
youer0219 <xlm20040219@qq.com>
2024-11-18 19:53:23 +0800

    组件/植物：基本实现植物头部组件
    还需要一个检测组件以及之配套的代码修改。目前都是永久循环，肯定不符合之后的需求。

--- Scenes/Entity/Plant/Components/PlantHeadComponent/plant_head_component.gd
+++ Scenes/Entity/Plant/Components/PlantHeadComponent/plant_head_component.gd
@@ -6,11 +6,14 @@ enum State {Idle,Attack}
 # 摇摆动画
 var sway_tween:Tween
 
-@export var gap_time:float = 0.16
+@export var gap_time:float = 0.25
+@export var attack_offect_angle:float = 45.0
 
 func _ready() -> void:
 	sway_tween = get_tree().create_tween().set_loops().bind_node(self)
+	attack_tween = get_tree().create_tween().bind_node(self).set_loops()
 	sway()
+	attack()
 
 func sway():
 	var curr_pos:= global_position
@@ -19,3 +22,9 @@ func sway():
 	sway_tween.tween_property(self,"global_position",curr_pos,gap_time)
 
 # 攻击动画
+var attack_tween:Tween
+func attack():
+	var curr_degerss = rotation_degrees
+	attack_tween.tween_property(self,"rotation_degrees",curr_degerss + attack_offect_angle, gap_time )
+	attack_tween.tween_property(self,"rotation_degrees",curr_degerss, gap_time)
+	attack_tween.tween_interval(gap_time * 2)
--- Scenes/Func/Fire/FireBulletComponent/fire_bullet_component.gd
+++ Scenes/Func/Fire/FireBulletComponent/fire_bullet_component.gd
@@ -5,14 +5,14 @@ extends Marker2D
 
 @onready var bullets:Node2D
 
+@export var fire_gap_time:float = 0.64
+
 const BULLET = preload("res://Scenes/Entity/Bullet/bullet.tscn")
 
 func _ready() -> void:
 	bullets = get_tree().get_first_node_in_group("Bullets")
 	var tween = get_tree().create_tween().set_loops()
-	tween.tween_callback(fire).set_delay(2.0)
-
-
+	tween.tween_callback(fire).set_delay(fire_gap_time)
 
 func fire():
 	var bullet = BULLET.instantiate()
