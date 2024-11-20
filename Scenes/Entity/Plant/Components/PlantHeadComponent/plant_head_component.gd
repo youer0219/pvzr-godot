114b5ab2efbdabc3bb9c89753a901484ee05411b
youer0219 <xlm20040219@qq.com>
2024-11-20 21:09:40 +0800

    组件/植物：改用着色器实现摇摆 + 删除摇摆代码 + 参数调整

--- Scenes/Entity/Plant/Components/PlantHeadComponent/plant_head_component.gd
+++ Scenes/Entity/Plant/Components/PlantHeadComponent/plant_head_component.gd
@@ -3,28 +3,22 @@ extends Node2D
 
 enum State {Idle,Attack}
 
-# 摇摆动画
-var sway_tween:Tween
 
 @export var gap_time:float = 0.25
-@export var attack_offect_angle:float = 45.0
+@export var attack_offect_angle:float = 60
+@export var fire_bullet_component:FireBulletComponent
 
 func _ready() -> void:
-	sway_tween = get_tree().create_tween().set_loops().bind_node(self)
-	attack_tween = get_tree().create_tween().bind_node(self).set_loops()
-	sway()
+	attack_tween = get_tree().create_tween().set_loops().bind_node(self)
 	attack()
-
-func sway():
-	var curr_pos:= global_position
-	sway_tween.tween_property(self,"global_position",curr_pos + Vector2(-0.5,0),gap_time)
-	sway_tween.tween_property(self,"global_position",curr_pos + Vector2(0,1),gap_time*2)
-	sway_tween.tween_property(self,"global_position",curr_pos,gap_time)
 
 # 攻击动画
 var attack_tween:Tween
 func attack():
 	var curr_degerss = rotation_degrees
-	attack_tween.tween_property(self,"rotation_degrees",curr_degerss + attack_offect_angle, gap_time )
-	attack_tween.tween_property(self,"rotation_degrees",curr_degerss, gap_time)
+	attack_tween.tween_interval(gap_time * 4)
+	attack_tween.tween_property(self,"rotation_degrees",curr_degerss + attack_offect_angle/2, gap_time/2 )
+	attack_tween.tween_callback(fire_bullet_component.fire)
+	attack_tween.tween_property(self,"rotation_degrees",curr_degerss + attack_offect_angle, gap_time/2 )
+	attack_tween.tween_property(self,"rotation_degrees",curr_degerss, gap_time )
 	attack_tween.tween_interval(gap_time * 2)
