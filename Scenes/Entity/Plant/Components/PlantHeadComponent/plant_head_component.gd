6aec613408f6bc1da73dd37e56f2f92e8ac073ba
youer0219 <xlm20040219@qq.com>
2024-12-09 22:29:13 +0800

    重构/组件：实现组件替换与向日葵发光

--- Scenes/Entity/Plant/Components/PlantHeadComponent/plant_head_component.gd
+++ Scenes/Entity/Plant/Components/PlantHeadComponent/plant_head_component.gd
@@ -3,9 +3,9 @@ extends Node2D
 @export var gap_time:float = 0.25
 @export var attack_offect_angle:float = 60
 @export var bullet_direction:Bullet.Direction
-@export var sway_sprite_component:SwaySpriteComponent
 @export var fire_bullet_component:FireBulletComponent
 @export var check_area_componet:CheckAreaComponent
+@export var visual_sprite_component: VisualSpriteComponent
 
 func _ready() -> void:
 	attack_tween = get_tree().create_tween().set_loops().bind_node(self)
@@ -15,7 +15,7 @@ func _ready() -> void:
 	check_area_componet.target_disappear.connect(stop_attack)
 	owner.change_direction.connect(
 		func(value:Bullet.Direction):
-			print(name," bullet_direction ", value)
+			#print(name," bullet_direction ", value)
 			bullet_direction = value
 	)
 
@@ -23,13 +23,13 @@ func _ready() -> void:
 # 攻击动画
 var attack_tween:Tween
 func attack():
-	var curr_degerss = sway_sprite_component.rotation_degrees
+	var curr_degerss = visual_sprite_component.rotation_degrees
 	attack_tween.tween_interval(gap_time * 4)
-	attack_tween.tween_property(sway_sprite_component,"rotation_degrees",curr_degerss + -1 * attack_offect_angle/2, gap_time/2 )
+	attack_tween.tween_property(visual_sprite_component,"rotation_degrees",curr_degerss + -1 * attack_offect_angle/2, gap_time/2 )
 	## 不要直接绑定bullet-direction数据，因为会固定，无法更改！
 	attack_tween.tween_callback(fire)
-	attack_tween.tween_property(sway_sprite_component,"rotation_degrees",curr_degerss + -1 * attack_offect_angle, gap_time/3 )
-	attack_tween.tween_property(sway_sprite_component,"rotation_degrees",curr_degerss, gap_time )
+	attack_tween.tween_property(visual_sprite_component,"rotation_degrees",curr_degerss + -1 * attack_offect_angle, gap_time/3 )
+	attack_tween.tween_property(visual_sprite_component,"rotation_degrees",curr_degerss, gap_time )
 	attack_tween.tween_interval(gap_time * 2)
 
 func fire():
--- Scenes/Entity/Plant/Components/SpriteComponent/visual_sprite_component.gd
+++ Scenes/Entity/Plant/Components/SpriteComponent/visual_sprite_component.gd
@@ -7,29 +7,45 @@ class_name VisualSpriteComponent
 @export var sway_speed:float = 3.0:set = set_sway_speed
 
 var blink_tween:Tween
-@export_range(0,1) var end_blink_intensity:float = 0.95
-@export var bline_time:float = 0.4
-@export var blink_all_time:float = 0.6
+@export_range(0,1) var end_blink_intensity:float = 0.9
+@export var bline_time:float = 0.3
+@export var blink_all_time:float = 0.45
 @export var gap_time:float = 2
+@export var is_blinking:bool = false:set = set_is_blinking
 var visual_material:ShaderMaterial
+
 
 func _ready() -> void:
 	visual_material = material
+	blink_tween = get_tree().create_tween().set_loops().bind_node(self)
+	blink_tween.stop()
+	is_blinking=false
 	set_bilnk_tween()
 
-
 func set_bilnk_tween():
-	blink_tween = get_tree().create_tween().set_loops().bind_node(self)
-	blink_tween.tween_method(SetShader_BlinkIntensity,0.0,end_blink_intensity,bline_time)
+	blink_tween.tween_method(set_blink_intensity,0.0,end_blink_intensity,bline_time)
 	blink_tween.tween_interval(blink_all_time)
-	blink_tween.tween_callback(
-		func():
-			material.set_shader_parameter("blink_intensity",0)
-			)
+	blink_tween.tween_callback(set_blink_intensity.bind(0))
 	blink_tween.tween_interval(gap_time)
 
-func SetShader_BlinkIntensity(new_value:float):
-	material.set_shader_parameter("blink_intensity",new_value)
+func set_blink_intensity(new_value:float):
+	visual_material.set_shader_parameter("blink_intensity",new_value)
+
+func start_blink():
+	if !blink_tween.is_running():
+		blink_tween.play()
+
+func stop_blink():
+	blink_tween.stop()
+
+func set_is_blinking(value:bool):
+	is_blinking = value
+	if !is_node_ready():
+		await ready
+	if is_blinking:
+		start_blink()
+	else:
+		stop_blink()
 
 func set_sway_shader():
 	visual_material.set_shader_parameter("strength",sway_strength)
