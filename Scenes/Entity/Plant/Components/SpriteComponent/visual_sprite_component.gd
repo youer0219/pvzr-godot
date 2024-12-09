dfcfa17486af1c25e9ec4b61bd839069337c7b54
youer0219 <xlm20040219@qq.com>
2024-12-09 20:30:52 +0800

    重构/组件：将发光和摇摆效果合一实现视觉纹理组件

--- Scenes/Entity/Plant/Components/SpriteComponent/visual_sprite_component.gd
+++ Scenes/Entity/Plant/Components/SpriteComponent/visual_sprite_component.gd
@@ -1,13 +1,20 @@
 extends Sprite2D
-class_name BlinkSpriteComponent
+class_name VisualSpriteComponent
+
+## 目前提供了'发光'和'顶部摇摆'的视觉效果
+
+@export var sway_strength:float = 2.0:set = set_sway_strength
+@export var sway_speed:float = 3.0:set = set_sway_speed
 
 var blink_tween:Tween
 @export_range(0,1) var end_blink_intensity:float = 0.95
 @export var bline_time:float = 0.4
 @export var blink_all_time:float = 0.6
 @export var gap_time:float = 2
+var visual_material:ShaderMaterial
 
 func _ready() -> void:
+	visual_material = material
 	set_bilnk_tween()
 
 
@@ -24,6 +31,18 @@ func set_bilnk_tween():
 func SetShader_BlinkIntensity(new_value:float):
 	material.set_shader_parameter("blink_intensity",new_value)
 
-func apply_effect():
-	
-	pass
+func set_sway_shader():
+	visual_material.set_shader_parameter("strength",sway_strength)
+	visual_material.set_shader_parameter("speed",sway_speed)
+
+func set_sway_strength(value:float):
+	sway_strength = value
+	if !is_node_ready():
+		await ready
+	set_sway_shader()
+
+func set_sway_speed(value:float):
+	sway_speed = value
+	if !is_node_ready():
+		await ready
+	set_sway_shader()
