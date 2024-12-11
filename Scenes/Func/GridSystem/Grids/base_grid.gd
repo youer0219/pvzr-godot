b63ab0b74efcefd6a87ac510975f4951d6fbf07b
youer0219 <xlm20040219@qq.com>
2024-12-11 11:30:11 +0800

    网格：实现网格位置显示与颜色、动画切换

--- Scenes/Func/GridSystem/Grids/base_grid.gd
+++ Scenes/Func/GridSystem/Grids/base_grid.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+class_name BaseGrid
 
 const black_frame = preload("res://Scenes/Func/GridSystem/Grids/assets/不适宜种植.png")
 const yellow_frame = preload("res://Scenes/Func/GridSystem/Grids/assets/适宜开炮.png")
@@ -10,10 +10,13 @@ const red_frame = preload("res://Scenes/Func/GridSystem/Grids/assets/适宜铲�
 @onready var up_right_frame: Sprite2D = $UpRightFrame
 @onready var down_right_frame: Sprite2D = $DownRightFrame
 @onready var down_left_frame: Sprite2D = $DownLeftFrame
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 enum ColorType {BLACK,YELLOW,GREEN,RED}
 @export var color_type:ColorType:set = set_color_type
 
+# 三种动画：无、强调-->消散
+@export var is_highlight:bool = false:set = set_is_highlight
 
 func _ready() -> void:
 	#color_type = ColorType.YELLOW
@@ -23,9 +26,19 @@ func _ready() -> void:
 	#color_type = ColorType.RED
 	pass
 
+# 消散函数
+
+func set_is_highlight(value:bool):
+	is_highlight = value
+	if !is_node_ready():
+		await ready
+	if is_highlight:
+		animation_player.play("highlight")
+	else:
+		animation_player.play("RESET")
+
 func set_glo_pos(glo_pos:Vector2):
 	global_position = glo_pos
-
 
 func set_color_type(value:ColorType):
 	color_type = value
--- Scenes/Func/GridSystem/grid_system.gd
+++ Scenes/Func/GridSystem/grid_system.gd
@@ -1,11 +1,27 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+## 作用：显示/不显示 不同形态的 网格
+
+## 与建造系统：
+## 为建造系统提供植物的鼠标落点
+## 建造系统为其提供形态参考
+var cell_size = Vector2i(16,16)
+@onready var base_grid: BaseGrid = $BaseGrid
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	#base_grid.color_type = BaseGrid.ColorType.BLACK
 	pass
+
+func _process(delta: float) -> void:
+	base_grid.position = get_mouse_cell_center()
+
+# 获取鼠标位置。得到鼠标对应cell的中心位置。
+func get_mouse_cell_center():
+	var mouse_pos = get_global_mouse_position()
+	var mouse_cell = floor(mouse_pos / Vector2(cell_size))
+	return get_cell_rect(mouse_cell).get_center()
+
+# 返回对应单元格的矩形
+func get_cell_rect(cell_pos:Vector2i) -> Rect2:
+	return Rect2(cell_pos * cell_size,cell_size)
