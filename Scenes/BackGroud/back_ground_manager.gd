c38d4098d1ad95b54c60e096a66d4a461da2f741
youer0219 <xlm20040219@qq.com>
2024-12-13 12:25:58 +0800

    背景：添加幻梦背景以及修复背景地图self-modulate失效的问题

--- Scenes/BackGroud/back_ground_manager.gd
+++ Scenes/BackGroud/back_ground_manager.gd
@@ -4,7 +4,8 @@ extends Node2D
 @onready var rain_rect: RainRect = $WeatherLayer/RainRect
 @onready var lightning_curtain:LightningCurtain = $LightningCurtain
 @onready var background_color: TextureRect = $Background/BackgroundColor
-@onready var background_map: TileMapLayer = $Background/ParallaxLayer/CanvasGroup3/BackgroundMap
+@onready var background_map: TileMapLayer = $Background/ParallaxLayer/BackgroundMapCanvasGroup/BackgroundMap
+@onready var background_map_canvas_group: CanvasGroup = $Background/ParallaxLayer/BackgroundMapCanvasGroup
 
 # 环境：白天 + 黑夜 + 红天 + 其他
 # 天气：雨 + 闪电
@@ -16,9 +17,11 @@ enum SkyState {Daytime,Nighttime,RedSky,Fantasy}
 const BLUE = preload("res://Scenes/BackGroud/Blue.tres")
 const NIGHT = preload("res://Scenes/BackGroud/Night.tres")
 const RED = preload("res://Scenes/BackGroud/Red.tres")
+const SUNSET = preload("res://Common/Shader/Fantasy/sunset.tres")
 
 func _ready() -> void:
 	sky_state = SkyState.values().pick_random()
+	#sky_state = SkyState.Nighttime
 
 
 func set_sky_state(value:SkyState):
@@ -29,20 +32,28 @@ func set_sky_state(value:SkyState):
 	rain_rect.is_raining = false
 	match sky_state:
 		SkyState.Daytime:
-			background_map.self_modulate = Color(1,1,1,1)
+			#background_map.self_modulate = Color(1,1,1,1)
+			background_map_canvas_group.modulate = Color(1,1,1,1)
 			background_color.texture = BLUE
+			background_color.material = null
 			rain_rect.is_raining = true if randf_range(0,1) < 0.5 else false
 		SkyState.Nighttime:
-			background_map.self_modulate = Color("adadad")
+			print("night-time")
+			#background_map.self_modulate = Color("adadad")
+			background_map_canvas_group.modulate = Color("adadad")
 			background_color.texture = NIGHT
+			background_color.material = null
 			lightning_curtain.is_runing = true if randf_range(0,1) < 0.5 else false
 			rain_rect.is_raining = true if randf_range(0,1) < 0.5 else false
 		SkyState.RedSky:
-			background_map.self_modulate = Color(1,1,1,1)
+			#background_map.self_modulate = Color(1,1,1,1)
+			background_map_canvas_group.modulate = Color(1,1,1,1)
 			background_color.texture = RED
+			background_color.material = null
 		SkyState.Fantasy:
-			background_map.self_modulate = Color(1,1,1,1)
-			background_color.texture = null
+			#background_map.self_modulate = Color(1,1,1,1)
+			background_map_canvas_group.modulate = Color(1,1,1,1)
+			background_color.material = SUNSET 
 
 
 	
