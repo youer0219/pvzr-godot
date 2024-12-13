f6c06af9039d041b478d5787237827ead520dcc7
youer0219 <xlm20040219@qq.com>
2024-12-13 22:27:40 +0800

    实体/子弹：实现一个有跟随功能的尖刺子弹

---
+++ Scenes/Entity/Bullet/CatnipBullet/catnip_bullet.gd
@@ -0,0 +1,8 @@
+extends Bullet
+
+
+func _ready() -> void:
+	super()
+	if bullet_move is BulletTrackingMove:
+		bullet_move.curr_body = self
+		bullet_move.target_body = get_tree().get_first_node_in_group("Dave")
---
+++ Scenes/Func/Move/BulletMove/BulletTrackingMove/bullet_tracking_move.gd
@@ -0,0 +1,31 @@
+extends BulletMove
+class_name BulletTrackingMove
+
+@export var turn_speed:float=200
+var current_direction:Vector2
+var curr_body:Node2D
+var target_body:Node2D
+var target_direction:Vector2 
+
+var is_rotating:bool=false
+var is_clockwise:bool=true
+var target_angle:float
+var angle_difference:float
+
+## TODO:需要实现有无敌人下的不同移动模式
+
+func _process(delta: float) -> void:
+	rotate_move(delta)
+
+
+func rotate_move(delta:float):
+	if !curr_body:return
+	curr_body.position+=current_direction*speed*delta
+	target_direction=(target_body.position - curr_body.position).normalized()
+	current_direction= Vector2.UP.rotated(curr_body.rotation) #获取当前方向矢量值
+	var error_value=delta*10
+	angle_difference=rad_to_deg(current_direction.angle_to(target_direction))
+	if(angle_difference>error_value): #根据角度正转还是反转
+		curr_body.rotation_degrees+=turn_speed*delta
+	elif(angle_difference<-error_value):
+		curr_body.rotation_degrees-=turn_speed*delta
